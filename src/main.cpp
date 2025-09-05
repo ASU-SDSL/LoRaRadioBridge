4d253d0999985ae315e16a9baeec0356fdd8ced4
Tyler Nielsen <tylernielsenn@gmail.com>
2025-09-04 22:46:26 -0700

    autoformatted

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,95 @@
 
-#include "Arduino.h"
 #include <RadioLib.h>
+
+#include "Arduino.h"
 #include "radio_interface.h"
 
 // states
 typedef enum : uint8_t {
   IDLE,
-  DETECTING, 
-  DECODE, 
+  DETECTING,
+  DECODE,
   TRANSMITTING,
   ADAPT,
   RECEIVING,
   FORWARD
-} sm_t; 
+} sm_t;
 
-sm_t state = IDLE; 
-int res = 0;  
+sm_t state = IDLE;
+int res = 0;
 
-
-void setup(){
+void setup() {
   // the init state
-  Serial.begin(115200); 
-  while(!Serial)
-    ;
+  Serial.begin(115200);
+  while (!Serial);
 
-  initRadio(); 
+  initRadio();
 }
 
-void loop(){
-  sm_t next_state = state; 
+void loop() {
+  sm_t next_state = state;
 
-  switch(state){
-    
+  switch (state) {
     case IDLE:
-      if(serial_input()){
-        next_state = DECODE; 
+      if (serial_input()) {
+        next_state = DECODE;
       } else {
-        startActivityDetection(); 
-        next_state = DETECTING; 
+        startActivityDetection();
+        next_state = DETECTING;
       }
       break;
 
     case DETECTING:
-      if(scan_timeout()){
-        next_state = IDLE; 
-      } else if(activity_detected()){
-        startReceive(); 
-        next_state = RECEIVING; 
+      if (scan_timeout()) {
+        next_state = IDLE;
+      } else if (activity_detected()) {
+        startReceive();
+        next_state = RECEIVING;
       }
-      break; 
+      break;
 
     case RECEIVING:
-      if(receive_done()){
-        next_state = FORWARD; 
-      } else if(receive_timeout()){
-        next_state = IDLE; 
+      if (receive_done()) {
+        next_state = FORWARD;
+      } else if (receive_timeout()) {
+        next_state = IDLE;
       } else {
-        next_state = RECEIVING; 
+        next_state = RECEIVING;
       }
       break;
 
     case FORWARD:
-      receiveAndForward(); 
-      next_state = IDLE;       
-      break; 
+      receiveAndForward();
+      next_state = IDLE;
+      break;
 
     case DECODE:
-      decodeAndTransmit(); 
-      next_state = TRANSMITTING; 
+      decodeAndTransmit();
+      next_state = TRANSMITTING;
       break;
 
     case TRANSMITTING:
-      if(transmit_done()){
-        transmitCleanUp(); 
-        next_state = ADAPT; 
-      } else if(transmit_timeout()){
-        transmitCleanUp(); 
-        next_state = IDLE; 
+      if (transmit_done()) {
+        transmitCleanUp();
+        next_state = ADAPT;
+      } else if (transmit_timeout()) {
+        transmitCleanUp();
+        next_state = IDLE;
       } else {
-        next_state = TRANSMITTING; 
+        next_state = TRANSMITTING;
       }
       break;
 
     case ADAPT:
-      adaptRadio(); 
-      next_state = IDLE; 
+      adaptRadio();
+      next_state = IDLE;
       break;
-    
-    default:
-      Serial.print("Bad state: "); Serial.println(state); 
-      break; 
 
+    default:
+      Serial.print("Bad state: ");
+      Serial.println(state);
+      break;
   }
 
-  state = next_state; 
-
+  state = next_state;
 }
--- src/radio_interface.cpp
+++ src/radio_interface.cpp
@@ -4,11 +4,12 @@
  * @brief Implementation of the radio_interface.h functions for the RFM98PW
  * @version 0.1
  * @date 2025-09-04
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
 #include "radio_interface.h"
+
 #include <RadioLib.h>
 
 // constants and settings
@@ -18,11 +19,11 @@
 #define RADIO_RFM_NRST_PIN 9
 #define RADIO_RFM_DIO1_PIN 3
 
-// Consistent radio settings: 
+// Consistent radio settings:
 #define RADIO_FREQ 434.0
 #define RADIO_SYNC_WORD 18
 #define RADIO_PREAMBLE_LEN 8
-#define RADIO_RFM_GAIN 0      // (auto)
+#define RADIO_RFM_GAIN 0  // (auto)
 #define RADIO_TRANSMIT_POWER 21
 
 // LoRa Modes:
@@ -36,136 +37,117 @@
 #define RADIO_SF_SAFE 10
 #define RADIO_CR_SAFE 5
 
-// shared variables 
-static RFM98 radio = new Module(RADIO_RFM_NSS_PIN, RADIO_RFM_DIO0_PIN, RADIO_RFM_NRST_PIN, RADIO_RFM_DIO1_PIN); 
-static int res = RADIOLIB_ERR_NONE; 
+// shared variables
+static RFM98 radio = new Module(RADIO_RFM_NSS_PIN, RADIO_RFM_DIO0_PIN,
+                                RADIO_RFM_NRST_PIN, RADIO_RFM_DIO1_PIN);
+static int res = RADIOLIB_ERR_NONE;
 
-// flags 
-static volatile bool operation_done_flag = false; 
-static volatile bool detected_flag = false; 
+// flags
+static volatile bool operation_done_flag = false;
+static volatile bool detected_flag = false;
 
-// isrs 
-static void raiseOperationDoneFlag(){
-  operation_done_flag = true; 
-}
-static bool popOperationDoneFlag(){
-  if(operation_done_flag){
+// isrs
+static void raiseOperationDoneFlag() { operation_done_flag = true; }
+static bool popOperationDoneFlag() {
+  if (operation_done_flag) {
     operation_done_flag = false;
-    return true; 
+    return true;
   } else {
-    return false; 
+    return false;
   }
 }
 
-static void raiseDetectedFlag(){
-  detected_flag = true; 
-}
-static bool popDetectedFlag(){
-  if(detected_flag){
-    detected_flag = false; 
-    return true; 
+static void raiseDetectedFlag() { detected_flag = true; }
+static bool popDetectedFlag() {
+  if (detected_flag) {
+    detected_flag = false;
+    return true;
   } else {
-    return false; 
+    return false;
   }
 }
 
 // status checkers (can be functions or just flags set from interrupt)
-// they should clean up after they return the status 
-bool serial_input(){
-  return Serial.available() > 0; 
-} 
+// they should clean up after they return the status
+bool serial_input() { return Serial.available() > 0; }
 
-bool scan_timeout(){
-  return popOperationDoneFlag(); 
-} 
+bool scan_timeout() { return popOperationDoneFlag(); }
 
-bool activity_detected(){
-  return popDetectedFlag(); 
-}
+bool activity_detected() { return popDetectedFlag(); }
 
-bool receive_done(){
-  return popOperationDoneFlag();  
-}
+bool receive_done() { return popOperationDoneFlag(); }
 
-bool transmit_done(){
-  return popOperationDoneFlag(); 
-} 
+bool transmit_done() { return popOperationDoneFlag(); }
 
-bool receive_timeout(){
-  return false;
-}
+bool receive_timeout() { return false; }
 
-bool transmit_timeout(){
-  return false; 
-} 
+bool transmit_timeout() { return false; }
 
-// operations 
-void initRadio(){
-  res = radio.begin(RADIO_FREQ, RADIO_BW_FAST, RADIO_SF_FAST, RADIO_CR_FAST, RADIO_SYNC_WORD, RADIO_TRANSMIT_POWER, RADIO_PREAMBLE_LEN, RADIO_RFM_GAIN);
+// operations
+void initRadio() {
+  res = radio.begin(RADIO_FREQ, RADIO_BW_FAST, RADIO_SF_FAST, RADIO_CR_FAST,
+                    RADIO_SYNC_WORD, RADIO_TRANSMIT_POWER, RADIO_PREAMBLE_LEN,
+                    RADIO_RFM_GAIN);
 
-  if(res != RADIOLIB_ERR_NONE){
+  if (res != RADIOLIB_ERR_NONE) {
     Serial.print(F("radio.begin failed, code "));
     Serial.println(res);
-    while (true)
-      ;
+    while (true);
   }
-  
+
   radio.setDio0Action(raiseOperationDoneFlag, RISING);
   radio.setDio1Action(raiseDetectedFlag, RISING);
 }
 
-void startActivityDetection(){
-  res = radio.startChannelScan(); 
+void startActivityDetection() {
+  res = radio.startChannelScan();
 
-  if(res != RADIOLIB_ERR_NONE){
+  if (res != RADIOLIB_ERR_NONE) {
     Serial.print(F("radio.startChannelScan failed, code "));
     Serial.println(res);
-    while (true)
-      ;
+    while (true);
   }
 }
 
-void startReceive(){
-  res = radio.startReceive(); 
+void startReceive() {
+  res = radio.startReceive();
 
-  if(res != RADIOLIB_ERR_NONE){
+  if (res != RADIOLIB_ERR_NONE) {
     Serial.print(F("radio.startReceive failed, code "));
     Serial.println(res);
-    while (true)
-      ;
+    while (true);
   }
-} 
-
-void receiveAndForward(){
-  size_t len = radio.getPacketLength(); 
-  uint8_t buf[len]; 
-  res = radio.readData(buf, len); 
-
-  if(res == RADIOLIB_ERR_NONE){
-    Serial.write(buf, len); 
-  }
-} 
-
-void decodeAndTransmit(){
-  // decode - TODO: make this smarter 
-  uint8_t buf[256]; 
-  size_t len = 0; 
-  
-  // while there are still bytes to send
-  while(Serial.available() > 0 && len < 256){
-    // read them into the buffer
-    buf[len++] = Serial.read(); 
-  }
-  // transmit the entire buffer
-  radio.startTransmit(buf, len); 
-
-} 
-
-void transmitCleanUp(){
-  radio.finishTransmit(); 
 }
 
-void adaptRadio(){
-  // use with decode 
-} 
+void receiveAndForward() {
+  size_t len = radio.getPacketLength();
+  uint8_t buf[len];
+  res = radio.readData(buf, len);
 
+  if (res == RADIOLIB_ERR_NONE) {
+    Serial.write(buf, len);
+  }
+}
+
+void decodeAndTransmit() {
+  // decode - TODO: make this smarter
+  uint8_t buf[256];
+  size_t len = 0;
+
+  // while there are still bytes to send
+  while (Serial.available() > 0 && len < 256) {
+    // read them into the buffer
+    buf[len++] = Serial.read();
+  }
+  // transmit the entire buffer
+  radio.startTransmit(buf, len);
+}
+
+void transmitCleanUp() {
+  // clean up needed after every transmit
+  radio.finishTransmit();
+}
+
+void adaptRadio() {
+  // use with decode
+}
